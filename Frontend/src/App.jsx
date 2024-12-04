9ec49bc77bef17c3059323867e957f4506b586dd
Bilal-Raza-SWE <bilalrazaswe@gmail.com>
2024-12-05 02:20:07 +0500

    adding dependiencies and resolving errors

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import Sidebar from "./components/Dashboard/sidebar";
+import WeatherDashboard from "./components/Dashboard/Weather-dashboard";
 const App = () => {
     return (
         <>
           <Sidebar /> 
+          <WeatherDashboard />
         </>
     );
 };
